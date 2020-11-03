a4f34677899ecd17d6933a1e9a560f1fbceb47a2
Yue Wang <yuewang0809@Yues-MBP.fios-router.home>
2020-11-03 00:36:59 -0800

    v

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,64 @@
 console.log('Hello from js/scripts.js!');
+
+const customName = document.getElementById('customname');
+const customLast = document.getElementById('customlast');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = 'According to the Island Weather Podcast, it was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['Tom Nook','Zucker','Raymond'];
+let insertY = ['the Nooks Cranny','The Able Sisters','the Museum'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob',name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature);
+    newStory = newStory.replace('300 pounds',weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
+
+$(document).ready(function() {
+  $('.randomize').click(function () {
+
+    if($('.randomize').is('click')){
+
+      $('.reset').hide();
+    }
+
+    else {
+      $('.reset').show();
+    }
+  });
+});
+
+function refreshPage(){
+  window.location.reload();
+}
